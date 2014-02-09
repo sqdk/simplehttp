8edf40302fa8a34e250584f0bc9715037dbbedb9
Michael Banzon <michael@banzon.dk>
2014-02-09 10:42:50 +0100

    Doc comment.

--- simplehttp.go
+++ simplehttp.go
@@ -93,6 +93,7 @@ func (r *SimpleHTTPRequest) MakeJSONRequest(v interface{}) error {
 	return json.Unmarshal(responseBody, v)
 }
 
+// Makes the prepared request and tries to unmarshal the result to the supplied interface.
 func (r *SimpleHTTPRequest) MakeXMLRequest(v interface{}) error {
 	responseBody, err := r.MakeRequest()
 	if err != nil {
