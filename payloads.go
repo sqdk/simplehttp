554e838c5312fab9d822949c726311d192ca95c0
Michael Banzon <michael@banzon.dk>
2014-03-11 20:53:33 +0100

    Added content for payloads skeletons.

--- payloads.go
+++ payloads.go
@@ -1,10 +1,14 @@
 package simplehttp
 
 type FormDataPayload struct {
+	Values map[string]string
+	Files  map[string]string
 }
 
 type UrlEncodedPayload struct {
+	Values map[string]string
 }
 
 type RawPayload struct {
+	Data []byte
 }
