f527c7dfed84c47ed019a9be23a653cda7f2b3ee
Michael Banzon <michael@banzon.dk>
2014-03-16 00:07:38 +0100

    Added some helper methods.

---
+++ helpers.go
@@ -0,0 +1,20 @@
+package simplehttp
+
+import "fmt"
+
+func (r *HTTPRequest) GetResponseFromJSON(v interface{}) error {
+	response, err := r.MakeGetRequest()
+	if err != nil {
+		return err
+	}
+	return response.ParseFromJSON(v)
+}
+
+func (r *HTTPRequest) PostResponseFromJSON(payload Payload, v interface{}) error {
+	response, err := r.MakePostRequest(payload)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(response.Data))
+	return response.ParseFromJSON(v)
+}
