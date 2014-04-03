82db7ef9739542fbfac5abafb0866087e8890276
Michael Banzon <michael@banzon.dk>
2014-04-03 18:56:15 +0200

    Made some changes to make it more javascript-like.

--- shorthand.go
+++ shorthand.go
@@ -1,27 +1,40 @@
 package simplehttp
 
-import "fmt"
-
 type BasicAuthentication struct {
 	User     string
 	Password string
 }
 
+type shorthandResponseHandler func([]byte)
+
 type Request struct {
 	Url            string
-	Method         string
 	Authentication BasicAuthentication
+	UserAgent      string
 }
 
-func (r Request) Do() {
-	fmt.Printf("%#v\n", r.Authentication)
-
-	if r.Method == "GET" {
-		doGet(r)
+func createHttpRequest(req Request) *HTTPRequest {
+	r := NewHTTPRequest(req.Url)
+	if req.Authentication.User != "" {
+		r.SetBasicAuth(req.Authentication.User, req.Authentication.Password)
 	}
+	if req.UserAgent != "" {
+		r.AddHeader("User-Agent", req.UserAgent)
+	}
+	return r
 }
 
-func doGet(r Request) {
-	req := NewHTTPRequest(r.Url)
-	req.MakeGetRequest()
+func (r Request) Get(success, failure shorthandResponseHandler) {
+	req := createHttpRequest(r)
+	res, err := req.MakeGetRequest()
+
+	if err != nil || res.Code >= 400 {
+		if failure != nil {
+			failure(res.Data)
+		}
+	} else {
+		if success != nil {
+			success(res.Data)
+		}
+	}
 }
--- shorthand_test.go
+++ shorthand_test.go
@@ -4,13 +4,12 @@ import (
 	"testing"
 )
 
-const (
-	simpleGetReturn = "Welcome to the real world!"
-)
-
 func TestSimpleGet(t *testing.T) {
 	Request{
-		Url:    "http://localhost:8888/",
-		Method: "GET",
-	}.Do()
+		Url: "http://localhost:4000/foobar",
+	}.Get(func(data []byte) {
+		t.Log(string(data))
+	}, func(data []byte) {
+		t.Log(string(data))
+	})
 }
