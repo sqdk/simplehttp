182315096bc57174b7ba535442732a31135428ca
Michael Banzon <michael@banzon.dk>
2014-04-17 15:37:56 +0200

    Wrapped test to require environment variable.

--- shorthand_test.go
+++ shorthand_test.go
@@ -1,25 +1,38 @@
 package simplehttp
 
 import (
+	"os"
 	"testing"
 )
 
+func reqEnv(key string, cb func(string)) {
+	value := os.Getenv(key)
+	if value != "" && cb != nil {
+		cb(value)
+	}
+}
+
 func TestSimpleGet(t *testing.T) {
-	Request{
-		Url: "http://localhost:4000/foobar",
-	}.Get(func(data []byte) {
-		t.Log(string(data))
-	}, func(data []byte) {
-		t.Log(string(data))
+	reqEnv("SIMPLEHTTP_TEST_SERVER", func(server string) {
+		Request{
+			Url: "http://" + server + "/get",
+		}.Get(func(data []byte) {
+			t.Log(string(data))
+		}, func(data []byte) {
+			t.Log(string(data))
+		})
 	})
 }
 
 func TestSimplePost(t *testing.T) {
-	Request{
-		Url: "http://localhost:4000/foobar",
-	}.Post(func(data []byte) {
-		t.Log(string(data))
-	}, func(data []byte) {
-		t.Log(string(data))
+	reqEnv("SIMPLEHTTP_TEST_SERVER", func(server string) {
+		Request{
+			Url:  "http://" + server + "/post",
+			Data: []byte("some data"),
+		}.Post(func(data []byte) {
+			t.Log(string(data))
+		}, func(data []byte) {
+			t.Log(string(data))
+		})
 	})
 }
